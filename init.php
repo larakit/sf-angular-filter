76ec4ea9b6f088f850f7e2a033264a051d629cee
Alexey Berdnikov <aberdnikov@gmail.com>
2016-07-11 21:42:01 +0500

    fixed

--- init.php
+++ init.php
@@ -1,4 +1,5 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-filter')
     ->usePackage('larakit/sf-angular')
-    ->js('//cdnjs.cloudflare.com/ajax/libs/angular-filter/0.5.8/angular-filter.min.js');
+    ->setSourceDir('public')
+    ->jsPackage('angular-filter.min.js');
