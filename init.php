456dacaa7578215a9ec8a657df79e10083f86eca
Berdnikov Alexey <berdnikov_ay@taximaxim.ru>
2017-01-11 16:09:09 +0500

    fixed

--- init.php
+++ init.php
@@ -2,4 +2,5 @@
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-filter')
     ->usePackage('larakit/sf-angular')
     ->setSourceDir('public')
+    ->ngModule('angular.filter')
     ->jsPackage('angular-filter.min.js');
