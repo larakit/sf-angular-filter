91ab7ccdd59f57a775c659194ce99e7691a1988e
Berdnikov Alexey <aberdnikov@gmail.com>
2017-04-20 09:02:06 +0500

    Update init.php

--- init.php
+++ init.php
@@ -2,5 +2,4 @@
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-filter')
     ->usePackage('larakit/sf-angular')
     ->setSourceDir('public')
-    ->ngModule('angular.filter')
     ->jsPackage('angular-filter.min.js');
